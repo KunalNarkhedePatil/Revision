c2434a32bee9b97c2871d760ca21d796f6078ca2
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2024-11-30 01:27:29 +0530

    c

---
+++ ProblemsOnNumbers/SumOfTwoNumber.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue1=0;
+    int iValue2=0;
+
+    cout<<"Enter the Value1"<<endl;
+    cin>>iValue1;
+
+    cout<<"Enter the Value2"<<endl;
+    cin>>iValue2;
+
+
+    int iSum=0;
+
+    iSum=iValue1+iValue2;
+
+    cout<<"Sum of two number is "<<iSum<<endl;
+}
